f85ff856611a44fe22968eaf2ef3f757ef38ede3
[2기-2반] 이재찬 <ljaechan1928@gmail.com>
2021-08-18 10:02:24 +0900

    add 1316.py 18.8.21

---
+++ 문자열/1316.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+for _ in range(N):
+    S = input()
+    for i in range(1, len(S)):
+        if S.find(S[i-1]) > S.find(S[i]):
+            N -= 1
+            break
+
+print(N)
